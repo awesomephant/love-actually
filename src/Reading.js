b88eff6a8af671611cf3760bcbd3440c83b605af
Max Kohler <max@koehler-kn.de>
2020-11-02 16:19:33 +0000

    Init netlify cms

--- src/Reading.js
+++ src/Reading.js
@@ -2,12 +2,16 @@ import React from "react";
 
 function Reading(props) {
   return (
+    <section className="reading">
     <nav className="reading--nav">
       <button>history</button>
       <button>readings</button>
       <button>recipes</button>
       <button>gallery</button>
     </nav>
+    <main className="reading-content">
+    </main>
+    </section>
   );
 }
 
