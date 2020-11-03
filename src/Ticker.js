b048ab57af46f01c52ee5500be3bdb516e469088
Max Kohler <max@koehler-kn.de>
2020-11-03 19:38:51 +0000

    Improve marquee

--- src/Ticker.js
+++ src/Ticker.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 function Ticker(props) {
+  const lastText = useRef(null);
+  const container = useRef(null);
+
   const innerStyle = {
-    animationDuration: `${props.text.length / 1.5}s`,
-  };
+    animationDuration: `${props.text.length * 100}ms`
+  }
+
+  useEffect(() => {
+    const containerBox = container.current.getBoundingClientRect();
+    const lastTextBox = lastText.current.getBoundingClientRect();
+    const ratio = lastTextBox.x / containerBox.width;
+    document.body.style.setProperty("--ticker-ratio", ratio);
+    console.log(ratio);
+  }, []);
+
   return (
     <div className="ticker" data-direction={props.direction}>
-      <p className="ticker-inner" style={innerStyle}>
+      <p ref={container} className="ticker-inner" style={innerStyle}>
         <span>{props.text}</span>
-        <span>{props.text}</span>
+        <span ref={lastText}>{props.text}</span>
       </p>
     </div>
   );
